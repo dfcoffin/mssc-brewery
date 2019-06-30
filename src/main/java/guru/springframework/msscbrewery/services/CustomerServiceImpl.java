9bd213648b99a701e0bfd159850b7603abc14a49
Donald F Coffin <donald.coffin@reminetworks.com>
2019-06-29 23:53:06 -0400

    Complete Customer MVC coding assignment

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -9,6 +10,7 @@ import java.util.UUID;
  * Created by Donald F. Coffin on 06/29/2019 at 16:27
  */
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
@@ -18,5 +20,23 @@ public class CustomerServiceImpl implements CustomerService {
                 .id(UUID.randomUUID())
                 .name("Joe Buck")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //TODO IMPL - Would add a real impl to update customer
+        log.debug("Updating....");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting....");
     }
 }
